4075a7aade1419f4806d6451c84f527023cd3d05
utsav8143 <utsav8143@gmail.com>
2026-07-25 19:31:14 +0530

    Layout Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import Header from './layout/Header.jsx'
-import Hero from './components/Hero.jsx'
+import { useState } from 'react';
+import Header from './layout/Header.jsx';
+import Hero from './components/Hero.jsx';
+import About from './components/About.jsx';
+import Programmes from './components/Programmes.jsx';
+import DayInSchool from './components/DayInSchool.jsx';
+import NewsEvents from './components/NewsEvent.jsx';
+import AdmissionsCTA from './components/Admissions.jsx';
+import Footer from './layout/Footer.jsx';
+import MobileDrawer from './layout/MobileDrawer.jsx';
 
-const App = () => {
+export default function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <div>
-      <Header/>
-      <Hero/>
-    </div>
-  )
-}
+    <>
+      <a href="#main" className="skip-link">Skip to main content</a>
 
-export default App
+      <MobileDrawer isOpen={drawerOpen} onClose={() => setDrawerOpen(false)} />
+      <Header onMenuOpen={() => setDrawerOpen(true)} />
+
+      <main id="main">
+        <Hero />
+        <About />
+        <Programmes />
+        <DayInSchool />
+        <NewsEvents />
+        <AdmissionsCTA />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
